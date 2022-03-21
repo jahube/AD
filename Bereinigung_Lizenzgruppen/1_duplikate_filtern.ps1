ba58a4ed15487069dd1f609fb4efa5b1a2674bfc
jahube <70081790+jahube@users.noreply.github.com>
2022-03-21 13:00:40 +0100

    Update 1_duplikate_filtern.ps1

--- Bereinigung_Lizenzgruppen/1_duplikate_filtern.ps1
+++ Bereinigung_Lizenzgruppen/1_duplikate_filtern.ps1
@@ -2,7 +2,7 @@ $Group_List1 =  "ELKW_O365_LIC_M3_Default","ELKW_O365_LIC_E3", "ELKW_O365_LIC_E3
 
 $MemberlistLizenzen = foreach ($licgroup in $Group_List1) { Get-ADGroupMember $licgroup }
 
-[System.Collections.ArrayList]$samaccountnames = $MemberlistLizenzen.samaccountname | sort-object -unique
+[System.Collections.ArrayList]$samaccountnames = $MemberlistLizenzen.SamAccountName | sort-object -unique
 
 $mbx = foreach ($mbx in $samaccountnames) { get-aduser $mbx -Properties mail,extensionattribute4,memberof,Distinguishedname }
 
@@ -12,8 +12,18 @@ $duplikate.count
 
 $Group_List2 =  "ELKW_O365_LIC_M3_Default","ELKW_O365_LIC_E3","ELKW_O365_LIC_E3_Default","ELKW_O365_LIC_E1_Default", "ELKW_O365_LIC_E3_Sekretaerinnen"
 
-$data = $duplikate | select mail,extensionattribute4,samaccountname,@{N="Top-Level_OU" ;E={ (($_.Distinguishedname.Split(','))[-4]) -replace "OU=" }},@{N="Lizenzgruppen" ;E={ (Get-ADPrincipalGroupMembership $_.SamAccountName |where { $_.Name -in $Group_List2 }).Name -join '|' }},@{N="memberof" ;E={ (($_.memberof.Split(','))[0]) -replace "cn=" -join ' | ' }},Distinguishedname
+$data = $duplikate | select mail,extensionattribute4,samaccountname,@{N="Top-Level_OU" ;E={ (($_.Distinguishedname.Split(','))[-4]) -replace "OU=" }},@{N="Lizenzgruppen" ;E={ (Get-ADPrincipalGroupMembership $_.SamAccountName |where { $_.Name -in $Group_List2 }).SamAccountName -join '|' }},@{N="memberof" ;E={ (($_.memberof.Split(','))[0]) -replace "cn=" -join ' | ' }},Distinguishedname
 
 $data |ft mail,extensionattribute4,samaccountname,Top-Level_OU,Lizenzgruppen,memberof
 
-foreach ($user in $data) { Remove-ADGroupMember -Identity ELKW_O365_LIC_E3 -Members $User.SamAccountName -Confirm:$false  }
+foreach ($user in $data) {
+
+foreach ($Group in ($User.Lizenzgruppen -Split '|')) {
+
+Remove-ADGroupMember -Identity $User.Lizenzgruppen -Members $User.SamAccountName -Confirm:$false 
+                                                     }
+                         }
+
+## end
+
+#foreach ($user in $data) { Remove-ADGroupMember -Identity ELKW_O365_LIC_E3 -Members $User.SamAccountName -Confirm:$false  }
