14b3f07e0252bb5135744bc84cf70833582b7f16
jahube <70081790+jahube@users.noreply.github.com>
2022-03-21 12:37:26 +0100

    Create 1_duplikate_filtern.ps1

---
+++ Bereinigung_Lizenzgruppen/1_duplikate_filtern.ps1
@@ -0,0 +1,19 @@
+$Group_List =  "ELKW_O365_LIC_M3_Default","ELKW_O365_LIC_E3", "ELKW_O365_LIC_E1_Default", "ELKW_O365_LIC_E3_Sekretaerinnen"
+
+$MemberlistLizenzen = foreach ($licgroup in $Group_List) { Get-ADGroupMember $licgroup }
+
+[System.Collections.ArrayList]$samaccountnames = $MemberlistLizenzen.samaccountname | sort-object -unique
+
+$mbx = foreach ($mbx in $samaccountnames) { get-aduser $mbx -Properties mail,extensionattribute4,memberof,Distinguishedname }
+
+$duplikate = $mbx.where({$_.extensionattribute4 -match "^AP-Typ"})
+
+$duplikate.count
+
+$Group_List =  "ELKW_O365_LIC_M3_Default","ELKW_O365_LIC_E3","ELKW_O365_LIC_E3_Default","ELKW_O365_LIC_E1_Default", "ELKW_O365_LIC_E3_Sekretaerinnen"
+
+$data = $duplikate | select mail,extensionattribute4,samaccountname,@{N="Top-Level_OU" ;E={ (($_.Distinguishedname.Split(','))[-4]) -replace "OU=" }},@{N="Lizenzgruppen" ;E={ (Get-ADPrincipalGroupMembership $_.SamAccountName |where { $_.Name -in $Group_List }).Name -join '|' }},@{N="memberof" ;E={ (($_.memberof.Split(','))[0]) -replace "cn=" -join ' | ' }},Distinguishedname
+
+$data |ft mail,extensionattribute4,samaccountname,Top-Level_OU,Lizenzgruppen,memberof
+
+foreach ($user in $data) { Remove-ADGroupMember -Identity ELKW_O365_LIC_E3 -Members $User.SamAccountName -Confirm:$false  }
