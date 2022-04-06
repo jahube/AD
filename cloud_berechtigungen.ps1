3ec807bf2613684441d15671cd0ea91d086b5bb4
jahube <70081790+jahube@users.noreply.github.com>
2022-04-06 07:19:34 +0200

    Update cloud_berechtigungen.ps1

--- cloud_berechtigungen.ps1
+++ cloud_berechtigungen.ps1
@@ -1,5 +1,17 @@
-$u = get-mailbox "User1_CLOUD@domain.de"
+$m = get-mailbox "FunktionCLOUD@domain.de"
+
+$u = get-mailuser "User1_ONPREM@domain.de"
+
+Add-recipientPermission $m.distinguishedname -Trustee $u.distinguishedname -AccessRights SendAs
+
+Get-recipientPermission $m.distinguishedname
+
+--------------------------------------------
 
 $m = get-mailuser "FunktionONPREM@domain.de"
 
-Add-recipientPermission $u.distinguishedname -Trustee $m.distinguishedname -AccessRights SendAs
+$u = get-mailbox "User1_CLOUD@domain.de"
+
+Add-recipientPermission $m.distinguishedname -Trustee $u.distinguishedname -AccessRights SendAs
+
+Get-recipientPermission $m.distinguishedname
