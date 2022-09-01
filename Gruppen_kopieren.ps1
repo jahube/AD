dcc09f52417222f651483d00c13a7b321205aeb5
jahube <70081790+jahube@users.noreply.github.com>
2022-09-01 17:25:27 +0200

    Create Gruppen_kopieren.ps1

---
+++ Gruppen_kopieren.ps1
@@ -0,0 +1,24 @@
+﻿
+$Vorlage_user = "Vorname.name"
+
+$user1 = "Vorname.name"
+
+$gruppen_vorlage = (get-aduser $Vorlage_user -Properties memberof).memberof
+
+$gruppen_User1= (get-aduser $user1 -Properties memberof).memberof
+
+$gruppen_vorlage.count
+
+$gruppen_User1.count
+
+foreach ($gruppe in $gruppen) {
+
+If(!($gruppe -in $gruppen_User1)) {
+
+Add-ADGroupMember $gruppe -Members Joerg.Mertin
+
+} }
+
+# check
+$gruppen_User1= (get-aduser $user1 -Properties memberof).memberof
+$gruppen_User1.count
